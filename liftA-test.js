8d6ad3eb5bfa8dba0be0ea39a74dfa63738953bf
Bill Enright <bill@yisup.com>
2017-09-14 12:07:39 -0700

    ifte, right, left, repeat, done constructors

--- liftA-test.js
+++ liftA-test.js
@@ -12,21 +12,18 @@ function add5(x) {
   return [x.first() + 5, x.second() - 5];
 }
 
-let add1Arr = aea.liftArrow(add1);
-let add2Arr = aea.liftArrow(add2);
-let add5Arr = aea.liftArrow(add5);
+let add1Arr = aea.liftA(add1);
+let add2Arr = aea.liftA(add2);
+let add5Arr = aea.liftA(add5);
 
-let add1Thenadd2 = aea.thenArrow(add1Arr, add2Arr);
-let add1Thenadd2Thenadd5 = aea.thenArrow(add1Thenadd2, add5Arr)
+let add1Thenadd2 = aea.thenA(add1Arr, add2Arr);
+let add1Thenadd2Thenadd5 = aea.thenA(add1Thenadd2, add5Arr)
 
-let add2Thenadd5 = aea.thenArrow(add2Arr, add5Arr);
-let add1Thenadd2Thenadd5v2 = aea.thenArrow(add1Arr, add2Thenadd5)
+let add2Thenadd5 = aea.thenA(add2Arr, add5Arr);
+let add1Thenadd2Thenadd5v2 = aea.thenA(add1Arr, add2Thenadd5)
 
 add1Thenadd2Thenadd5([1, 2], (x) => console.log('and the result is: ', x), aea.p)
 
-console.log(add1.liftA());
-console.log(add1.liftA().thenA(add2.liftA()));
-console.log(add1.liftA().thenA(add2.liftA()).thenA(add5.liftA()));
 console.log(add1.liftA().thenA(add2.liftA()).thenA(add5.liftA()).runA([1,2]));
 
 
@@ -41,45 +38,45 @@ add1.liftA()
   .thenA(add2.liftA().thenA(add5.liftA()))
   .runA([6,7]);
 //
-let testProductArrow = aea.productArrow(add1Arr, add5Arr);
+let testProductArrow = aea.productA(add1Arr, add5Arr);
 testProductArrow([[1,5],[2,6]], (x) => console.log('test product: ', x), aea.p);
 add1.liftA().productA(add5.liftA()).runA([[1,5],[2,6]]);
 //
-let fan1And5 = aea.fanArrow(add1Arr, add5Arr);
-let testFanArrow = aea.fanArrow(fan1And5, add2Arr);
+let fan1And5 = aea.fanA(add1Arr, add5Arr);
+let testFanArrow = aea.fanA(fan1And5, add2Arr);
 testFanArrow([6, 2], (x) => console.log('test fan: ', x), aea.p);
 add1.liftA().fanA(add5.liftA()).fanA(add2.liftA()).runA([6, 2])
 //
-let testFirstArrow = aea.firstArrow(aea.liftArrow((x) => x * 2))
+let testFirstArrow = aea.firstA(aea.liftA((x) => x * 2))
 testFirstArrow([2, "dave"], (x) => console.log('testFirstArrow: ', x), aea.p)// types for looping
 //
-let testSecondArrow = aea.secondArrow(aea.liftArrow((x) => x * 3))
+let testSecondArrow = aea.secondA(aea.liftA((x) => x * 3))
 testSecondArrow(["suzie creamcheese", 3], (x) => console.log('testSecondArrow: ', x), aea.p)// types for looping
 //
 function repeatAdd1Arrow(x, cont, p) {
-  setImmediate(() => {
+  setTimeout(() => {
     let first = x.first();
-    if (first === 1000000) {
-      cont([first, aea.ArrDone], p);
+    if (first === 1000) {
+      cont([first, aea.Done(first)], p);
     } else {
-      cont([first, aea.ArrRepeat], p);
+      cont([first, aea.Repeat(first)], p);
     }
-  });
+  }, 0);
 }
 //
-let frst = aea.firstArrow(aea.liftArrow((x) => x + 1))
-let repeatTest = aea.repeatArrow(aea.thenArrow(frst, repeatAdd1Arrow));
-repeatTest.runA([0, aea.ArrRepeat]);
+let frst = aea.firstA(aea.liftA((x) => x + 1))
+let repeatTest = aea.repeatA(aea.thenA(frst, repeatAdd1Arrow));
+repeatTest.runA([0, aea.Repeat()]);
 
-let testDelay = aea.thenArrow((x, cont, p) => { console.log('start delay 2000'); cont(x, p); }, aea.thenArrow(aea.delayArrow(2000), (x, cont, p) => {console.log('end delay 2000');}));
+let testDelay = aea.thenA((x, cont, p) => { console.log('start delay 2000'); cont(x, p); }, aea.thenA(aea.delayA(2000), (x, cont, p) => {console.log('end delay 2000');}));
 testDelay([1,2], () => {}, aea.p);
-let testCancelDelay = aea.thenArrow((x, cont, p) => { console.log('start delay 3000'); cont(x, p); }, aea.thenArrow(aea.delayArrow(3000), (x, cont, p) => {console.log('end delay 3000');}));
+let testCancelDelay = aea.thenA((x, cont, p) => { console.log('start delay 3000'); cont(x, p); }, aea.thenA(aea.delayA(3000), (x, cont, p) => {console.log('end delay 3000');}));
 cancelId = testCancelDelay([1,2], () => {}, aea.p);
 aea.p.cancel(cancelId);
 
 
 let stuff = aea
-	.secondArrow(
+	.secondA(
 		((x) => x + 2).liftA()
 	)
 	.thenA(
