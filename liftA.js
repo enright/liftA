ea3d50939edf3a63b3634697e4981025b2dbeb0a
Bill Enright <bill@yisup.com>
2018-10-28 21:54:05 -0700

    copyright update

--- liftA.js
+++ liftA.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017 Bill Enright
+Copyright (c) 2017-2018 Bill Enright
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,50 +20,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
-/*
-The Point of No Return
-
-This is the liftA.js library by Bill Enright. It is an implementation of
-'asynchronous event arrows' It is a library of function arrows for
-high-order functional programming of asynchronous and event-driven work
-in JavaScript. This work is an adaptation of the "Arrowlets" library by
-Khoo Yit Phang. It attempts to offer a simplified view of arrows so that
-you may build on it without being confined by the original style. This
-library has fewer features in order to be less opinionated.
-
-With the advent of ES6 implementations that support tail call
-optimization (such as the V8 engine) there is no longer a need to use a
-trampoline and thunking to achieve continuations, and this greatly
-simplifies the implementation of asynchronous event arrows in
-JavaScript. Also, the expressiveness of 'arrow function' syntax in ES6
-makes the code defining arrows much more readable.
-
-Arrows can be contructed from a variety of included functions, such as
-liftA(), thenA(), firstA(), secondA(), productA()
-and fanA(). For a visual description of these arrows see arr, >>>,
-first, second, *** and &&& here:
-https://en.wikibooks.org/wiki/Haskell/Understanding_arrows
-
-orA() executes only one of its arrow arguments (the first to complete - the
-other is cancelled). repeatA() allows for looping. bindA() and
-joinA() allow you to keep previous results in the data flowing
-through the arrows. They are similar to thenA(), but they combine
-output variations in a pair.
-
-This library augments Array with a first() and second() function to
-support simple semantics for handling tuples. Generally, we assume that
-the 'x' - the first parameter when calling a function arrow - is a pair.
-firstA() and secondA() construct arrows that operate on one of
-the pair, and pass the value of the other of the pair through.
-
-This library also augments Function with a number of useful functions
-for building more complex arrows with a fluent syntax. Hence the name of
-this library: liftA
-
-A rudimentary mechanism for cancelling arrows is provided.
-
-To Be Continued...
 */
 (function () {
   "use strict"; // enables proper tail calls (PTC)
